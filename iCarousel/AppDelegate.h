24de216b3599ae0e6bf07fab1fafe56a54b9c3f2
eyupcimen <cmneyp@gmail.com>
2017-01-28 14:23:59 +0300

    Initial Commit

---
+++ iCarousel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iCarousel
+//
+//  Created by eyupcimen on 28/01/2017.
+//  Copyright © 2017 eyupcimen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iCarousel/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iCarousel
+//
+//  Created by eyupcimen on 28/01/2017.
+//  Copyright © 2017 eyupcimen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
